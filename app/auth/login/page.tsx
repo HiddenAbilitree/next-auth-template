f1184993d87069df8b9b3b2f65c585cebe532a26
Eric Zhang <me@ericzhang.dev>
2025-04-01 09:15:08 -0400

    chore: update deps

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,7 @@
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+      Login Page
+    </div>
+  );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,21 @@
-import { betterFetch } from '@better-fetch/fetch';
-import type { auth } from '@/lib/auth';
-import { NextResponse, type NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { headers } from 'next/headers';
+import { auth } from '@/lib/auth';
 
-type Session = typeof auth.$Infer.Session;
+export async function middleware(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default async function authMiddleware(request: NextRequest) {
-  const { data: session } = await betterFetch<Session>(
-    '/api/auth/get-session',
-    {
-      baseURL: request.nextUrl.origin,
-      headers: {
-        cookie: request.headers.get('cookie') || '',
-      },
-    },
-  );
   if (!session) {
     return NextResponse.redirect(new URL('/auth/login', request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
+  runtime: 'nodejs',
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|auth).*)',
   ],
